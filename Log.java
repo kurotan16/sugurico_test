08da9240eae64198fcf554448676205b3585738e
kurotan16 <166577126+kurotan16@users.noreply.github.com>
2025-06-18 14:24:42 +0900

    ライフログ機能作成
    少しエラーが出る可能性はあるが動作はしているのでおそらく問題なし

---
+++ Log.java
@@ -0,0 +1,32 @@
+package com.example.suguriko.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "logs") // DBに"logs"テーブルが作られる
+public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1000) // 1000文字まで
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    // Logエンティティは「多」、Userエンティティは「一」の関係
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false) // user_idカラムでUserテーブルと連携
+    private User user;
+
+    // データが保存される直前に実行されるメソッド
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
